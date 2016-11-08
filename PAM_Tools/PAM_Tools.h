b72b7f97ee6358b72e64f03e94271b1aa52f4318
PI and more <piandmore@j-tools.net>
2016-11-08 14:00:08 +0100

    Updated documentation

--- PAM_Tools/PAM_Tools.h
+++ PAM_Tools/PAM_Tools.h
@@ -4,12 +4,16 @@
 //
 // If getStringKey does not find the key requested, this define is returned
 //
-#define NOKEYFOUND "XNOKEYFNDX"
+#define NOSTRINGKEYFOUND "XNOKEYFNDX"
+#define NOINTKEYFOUND -98989898
 
 #include <Arduino.h>
 #include <PAM_Defines.h>
 
 void toolsSetup ();
 String getStringKey (String, String);
+int getIntKey (String, String);
+void putStringKey (String, String, String);
+void putIntKey (String, String, int);
 
 #endif
